915791b2e1dd8d692fbd6bcbada5fd936ceecfda
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-03-23 19:47:05 -0300

    First Commit idk

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # friday-night-funkin-browser
- My recreation of Friday Night Funkin', made entirely with javascript in the browser!
+
+My recreation of Friday Night Funkin', made entirely with javascript in the browser!
---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from 'react';
+
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  
+
+  useEffect(() => {
+    
+  })
+
+  return (
+    <div className={styles.container}>
+      text
+      <div>Element moving</div>
+    </div>
+  )
+}
